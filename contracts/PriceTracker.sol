167a8b206a8a6518a68d558413d542bed01d0fa5
Sneh Koul <35871990+Sneh1999@users.noreply.github.com>
2021-02-08 21:04:33 -0500

    Update PriceTracker.sol

---
+++ contracts/PriceTracker.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.8.0;
+
+
+/** 
+ * @title PriceTracker 
+ * @dev Tracks Prices for various NFT's
+ */
+contract PriceTracker {
+    
+    
+    address owner;
+    mapping(uint8 => uint64) public nfts;
+    
+    constructor() public {
+    }
+    
+    event UpdatedPrice(uint8 id, uint64 oldPrice, uint64 newPrice, string message);
+    event GetPrice(uint8 id, uint64 price,string message);
+    // owner that it only the dao
+    
+    // update payment
+    function updatePrice(uint8 _nftId, uint64 _price) public {
+        // find the nft in the mapping 
+        // change the price of the nft
+        uint64  oldPrice = nfts[_nftId];
+        uint64 newPrice = _price;
+        require(oldPrice != newPrice, "Old Price and New Price are the same value");
+        nfts[_nftId] = newPrice;
+        emit UpdatedPrice(_nftId,oldPrice,newPrice,"Successfuly updated the price");
+        
+    }
+    
+    // get the price
+    function getPrice(uint8 _nftId) public {
+        uint64 price = nfts[_nftId];
+       require(price  != 0, "The given NFT doesnt exist");
+        emit GetPrice(_nftId, price, "Successfuly retrieved the Price");
+         
+    }
+    
+
+
+}
