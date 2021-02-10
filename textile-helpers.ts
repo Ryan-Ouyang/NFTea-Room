3919b35491da32c37c6927b33277d4063ed8bd46
Ryan Ouyang <ryan.ouyang01@gmail.com>
2021-02-10 01:19:03 -0500

    Login with Metamask + Textile integration

---
+++ textile-helpers.ts
@@ -0,0 +1,17 @@
+import { Client, PrivateKey, UserAuth, ThreadID, KeyInfo } from "@textile/hub";
+
+export const keyInfo: KeyInfo = {
+  key: "buyh2yjxxheyvgkd7zijwlcmm5m",
+  secret: "bp52erpfqyi5d72ttlorne4bdfenkuxxwytla2za",
+};
+
+export const schema = {
+  $schema: "http://json-schema.org/draft-07/schema#",
+  title: "Suggestion",
+  type: "object",
+  properties: {
+    _id: { type: "string" },
+    NFT_ID: { type: "string" },
+    new_price: { type: "number" },
+  },
+};
